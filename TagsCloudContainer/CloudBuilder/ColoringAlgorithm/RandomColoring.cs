64eac29f1ff204cf2727b0abfd733bc5967fc546
Илья Creewick Юхатский <creewick@outlook.com>
2017-12-04 00:26:10 +0500

    * RandomColoring fix

--- TagsCloudContainer/CloudBuilder/ColoringAlgorithm/RandomColoring.cs
+++ TagsCloudContainer/CloudBuilder/ColoringAlgorithm/RandomColoring.cs
@@ -7,9 +7,10 @@ namespace TagsCloudContainer
 {
     public class RandomColoring : IColoringAlgorithm
     {
+        private Random random = new Random();
+
         public Color GetColor(IEnumerable<Color> colors, string name, int frequency)
         {
-            var random = new Random();
             return colors.ElementAt(random.Next(colors.Count() - 1));
         }
     }
